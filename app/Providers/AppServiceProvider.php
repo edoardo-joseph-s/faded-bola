af7198ca746ca423815eadb330fc9dad05944d85
ulan sila <edoardosila07@gmail.com>
2026-07-22 06:58:39 +0700

    fix: force cookie session driver and create /tmp/sessions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Config::set('session.driver', 'cookie');
+
         @mkdir(config('view.compiled'), 0755, true);
+        @mkdir('/tmp/sessions', 0755, true);
 
         if (app()->runningInConsole()) {
             return;
