a7380604daf27afa1b69b2b0acd53eb5f31a895f
ulan sila <edoardosila07@gmail.com>
2026-07-22 07:16:52 +0700

    fix: PHP serves static files directly, bypass Vercel routes issue

--- api/index.php
+++ api/index.php
@@ -1,3 +1,34 @@
 <?php
 
-require __DIR__ .'/../public/index.php';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$publicPath = __DIR__ . '/../public' . $uri;
+
+if ($uri !== '/' && is_file($publicPath)) {
+    $mimeTypes = [
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'svg' => 'image/svg+xml',
+        'ico' => 'image/x-icon',
+        'webp' => 'image/webp',
+        'woff2' => 'font/woff2',
+        'woff' => 'font/woff',
+        'ttf' => 'font/ttf',
+        'json' => 'application/json',
+        'txt' => 'text/plain',
+        'xml' => 'application/xml',
+    ];
+
+    $ext = pathinfo($uri, PATHINFO_EXTENSION);
+    if (isset($mimeTypes[$ext])) {
+        header('Content-Type: ' . $mimeTypes[$ext]);
+    }
+
+    readfile($publicPath);
+    exit;
+}
+
+require __DIR__ . '/../public/index.php';
